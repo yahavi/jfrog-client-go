7c40cce3a81675dd3f87d921547bb0c91f8333a7
yahavi <yahavizhak@gmail.com>
2018-11-30 17:39:01 +0200

    Add Ubuntu to Appveyor tests

--- tests/artifactory_test.go
+++ tests/artifactory_test.go
@@ -5,11 +5,12 @@ import (
 )
 
 func TestGetArtifactoryVersion(t *testing.T) {
-	version, err := getArtDetails().GetVersion()
-	if err != nil {
-		t.Error(err)
-	}
-	if version == "" {
-		t.Error("Expected a version, got empty string")
-	}
+	//version, err := getArtDetails().GetVersion()
+	t.Fail()
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//if version == "" {
+	//	t.Error("Expected a version, got empty string")
+	//}
 }
